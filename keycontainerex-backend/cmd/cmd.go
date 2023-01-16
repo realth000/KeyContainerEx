520df22c0d8d3c8c7c6d1322254e4bd5bafd6e23
realth000 <a598919335@gmail.com>
2023-01-17 00:23:32 +0800

    feat(cmd): Add cmd init
    Add cmd init:
    * Add cmd init steps.

--- keycontainerex-backend/cmd/cmd.go
+++ keycontainerex-backend/cmd/cmd.go
@@ -1,9 +1,16 @@
 package cmd
 
 import (
+	"KeyContainerEx/log"
 	"fmt"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"os"
+	"time"
+)
+
+var (
+	defaultStoragePath = ""
 )
 
 const (
@@ -14,12 +21,36 @@ const (
 var rootCmd = &cobra.Command{
 	Use:   AppName,
 	Short: fmt.Sprintf("%s %s: Manage password offline and everywhere", AppName, AppVersion),
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		storagePath := ""
+		// Load config environment.
+
+		// Initialize storage.
+		if storagePath == "" {
+			storagePath = defaultStoragePath
+		}
+		exist, err := checkInit(storagePath)
+		if err != nil {
+			fmt.Println("failed to check init:", err)
+		}
+		if !exist {
+			err = initialize(storagePath)
+			if err != nil {
+			}
+		}
+		fmt.Println("root cmd persist pre run. exist =", exist)
+	},
 }
 
 var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "add password",
+	PreRun: func(cmd *cobra.Command, args []string) {
+
+		fmt.Println("add cmd pre run")
+	},
 	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("add cmd run")
 
 	},
 }
@@ -56,6 +87,54 @@ func init() {
 }
 
 func Run() {
+	cacheConfDir, err := os.UserCacheDir()
+	if err != nil {
+		log.DryErrorln("failed to get cache directory:", cacheConfDir)
+	} else {
+		logDirPath := fmt.Sprintf("%s/KeyContainerEx", cacheConfDir)
+		tmpDir, err := os.Stat(logDirPath)
+		if os.IsNotExist(err) {
+			if err = os.Mkdir(logDirPath, 0755); err != nil {
+				log.DryErrorln("failed to create log file directory")
+			}
+		} else if err == nil || os.IsExist(err) {
+			if !tmpDir.IsDir() {
+				if err = os.Remove(logDirPath); err != nil {
+					log.DryErrorln("failed to delete file:", err)
+				}
+				if err = os.Mkdir(logDirPath, 0755); err != nil {
+					log.DryErrorln("failed to create log file directory")
+				}
+			}
+		}
+		log.InitLogger(logrus.DebugLevel, fmt.Sprintf("%s/KeyContainerEx/log-%s.txt", cacheConfDir, time.Now().Format("20060102150405")))
+	}
+
+	userConfDir, err := os.UserConfigDir()
+	if err != nil {
+		fmt.Println("failed to get user config directory:", err)
+	} else {
+		confDirPath := fmt.Sprintf("%s/KeyContainerEx", userConfDir)
+		tmpDir, err := os.Stat(confDirPath)
+		if os.IsNotExist(err) {
+			if err = os.Mkdir(confDirPath, 0755); err != nil {
+				log.Fatalln("failed to create config dir:", err)
+			}
+		} else if err == nil || os.IsExist(err) {
+			if !tmpDir.IsDir() {
+				if err = os.Remove(confDirPath); err != nil {
+					log.Fatalln("failed to remove config file:", err)
+				}
+				if err = os.Mkdir(confDirPath, 0755); err != nil {
+					log.Fatalln("failed to create config directory:", err)
+				}
+			}
+		} else {
+			log.Fatalln("failed to check config dir:", err)
+		}
+		defaultStoragePath = fmt.Sprintf("%s/KeyContainerEx/storage/default.kcex", userConfDir)
+	}
+
 	if err := rootCmd.Execute(); err != nil {
 		_, _ = fmt.Fprint(os.Stderr, err)
 		os.Exit(1)
---
+++ keycontainerex-backend/cmd/cmd_init.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"KeyContainerEx/log"
+	"fmt"
+	"golang.org/x/crypto/ssh/terminal"
+	"os"
+)
+
+const (
+	stdin = 0
+)
+
+func checkInit(storagePath string) (bool, error) {
+	if storagePath == "" {
+		return false, fmt.Errorf("empty storage path")
+	}
+	info, err := os.Stat(storagePath)
+	if err == nil || os.IsExist(err) {
+		if info.IsDir() {
+			return false, fmt.Errorf("not a file: %s", storagePath)
+		}
+		// Validate storage.
+		return true, nil
+	} else if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func initialize(storagePath string) error {
+	fmt.Println("Initializing storage...")
+	fmt.Println("Input storage password:")
+	pw, err := terminal.ReadPassword(stdin)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Confirm storage password:")
+	cpw, err := terminal.ReadPassword(stdin)
+	if err != nil {
+		return err
+	}
+	spw := string(pw)
+	scpw := string(cpw)
+	if spw != scpw {
+		log.FatalPrintln("password and confirm password not the same, exit")
+	}
+	return nil
+}
