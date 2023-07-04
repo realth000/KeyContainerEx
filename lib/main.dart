7710ba4ea2b5fe8fa5d1d7a993de18d25a219875
realth000 <a598919335@gmail.com>
2023-07-05 02:11:04 +0800

    chore(gitignore): Update gitignore
    Update gitignore:
    * Add vendor

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // TODO: More code
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
